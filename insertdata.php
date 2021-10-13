f08e84dae1dbe8bfc7e95a357def754ca13cf7da
Administrator <admin@example.com>
2021-10-13 18:09:25 +0000

    Add new file

---
+++ insertdata.php
@@ -0,0 +1,34 @@
+<?php
+
+// include database 
+
+$servername = "localhost";
+$username = "developertes";
+$password = "HL@2021test";
+$dbname = "hla";
+
+//
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
+$user = 
+//table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
+$location = 
+
+
+
+if ($conn->query($user) === TRUE && $conn->query($location) === TRUE ) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
