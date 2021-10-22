c08daf6c241d445589446efc54cf640679d0e15a
developer-test <muhamadzahirhamzah@gmail.com>
2021-10-22 10:30:42 +0800

    Update code

--- insertdata.php
+++ insertdata.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
 }
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
-$user =  "INSERT INTO users(id, name, email, token, created) VALUES (1,'zahir', 'testing', 'zahir@gmail.com', 'ae2b1fca515949e5d54fb22b8ed95575', '2021-10-21 10:30:00') ";
+$user =  "INSERT INTO users(id, name, email, token, created) VALUES (1,'zahir', 'zahir@gmail.com', 'ae2b1fca515949e5d54fb22b8ed95575', '2021-10-21 10:30:00') ";
 
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
 $location = "INSERT INTO locations(id, user_id, district,state,country) VALUES (1,1,'Cheras','Selangor', 'Malaysia')";
