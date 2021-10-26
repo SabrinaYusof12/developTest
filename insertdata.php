f652eddcf4593e9588a0225e63771f1f040f77a2
Arief Rafie <binaryadh91@gmail.com>
2021-10-26 21:21:59 +0800

    upd test 3

--- insertdata.php
+++ insertdata.php
@@ -4,8 +4,8 @@
 
 $servername = "localhost";
 $username = "developertes";
-//$password = "HL@2021test";
-$password = "YES";
+$password = "HL@2021test";
+//$password = "YES";
 $dbname = "hla";
 
 //
@@ -18,10 +18,10 @@ if ($conn->connect_error) {
 }
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
-$user = "INSERT INTO users (users_id,users_name,users_email,users_token,created)VALUES('00004','Muhammad Arief Bin Mohd Rafie','binaryadh91@gmail.com','8915b9899cd4037aaf8f1980c39c75fa','26-10-2021 19:09')";
+$user = "INSERT INTO users (users_id,users_name,users_email,users_token,created)VALUES('000111'),'Muhammad Arief Bin Mohd Rafie','binaryadh91@gmail.com','8915b9899cd4037aaf8f1980c39c75fa','26-10-2021 21:20')";
 
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
-$location = "INSERT INTO location(location_id,user_id,location_district,location_state,location_country)VALUES(01,00004,'Pekan','Pahang','Malaysia')";
+$location = "INSERT INTO location(location_id,user_id,location_district,location_state,location_country)VALUES(01,000111,'Pekan','Pahang','Malaysia')";
 
 
 
