58b104166d5c0ee11cd60c42bfdb2215c2c20b33
developer-test <developer.test@hla-integrated.com>
2021-10-26 11:44:38 +0000

    Update insertdata.php

--- insertdata.php
+++ insertdata.php
@@ -6,7 +6,6 @@ $servername = "localhost";
 $username = "developertes";
 $password = "HL@2021test";
 $dbname = "hla";
-
 //
 
 // Create connection
@@ -17,9 +16,10 @@ if ($conn->connect_error) {
 }
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
-$user = 
+$user = "INSERT INTO users (user_id,users_name,users_email,users_token,created")VALUES('00006'),'Mashitah','shitahramli12@gmail.com','09b4105f39d01d8d1d835ef69896ae691b44c809','26-10-2021 19:20')";
+'
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
-$location = 
+$location = INSERT INTO location (location_id,user_id,location_district,location_state,location_country)VALUES(01,00004,'Pekan','Pahang',Malaysia')";
 
 
 
