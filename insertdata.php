e8b06021a2819629a14bfa33b537fdda23228ff1
developer-test <developer.test@hla-integrated.com>
2021-10-27 04:24:41 +0000

    Update index.php, insertdata.php files

--- insertdata.php
+++ insertdata.php
@@ -3,7 +3,7 @@
 // include database 
 
 $servername = "localhost";
-$username = "developertes";
+$username = "developertest";
 $password = "HL@2021test";
 $dbname = "hla";
 
@@ -17,9 +17,10 @@ if ($conn->connect_error) {
 }
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
-$user = 
+$user =  "INSERT INTO users(id,name, email, token, created) VALUES (10,'mohdhanafiz', 'mohdhanafizbah@gmail.com', 'd41d8cd98f00b204e9800998ecf8427e', '2021-10-27 11:53:00') ";
+
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
-$location = 
+$location = "INSERT INTO locations(id, user_id, district,state,country) VALUES (9,10,'Pekan','Pahang', 'Malaysia')";
 
 
 
