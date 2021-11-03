389c5b0d37f4e6c99e1925c7accf545418e4ce02
nadiashokkan <nadiashokkan1@gmail.com>
2021-11-03 17:29:15 +0800

    code

--- index.php
+++ index.php
@@ -27,20 +27,18 @@ foreach ($headers as $header => $value) {
 }
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
-$sql =  "INSERT INTO user (name,email,token,created)
-VALUES('nadia','nadiashokkan2@gmail.com','$token',NOW())";
+$sql =  "INSERT INTO user (id,name,email,token,created)
+VALUES(21,'nadia','nadiashokkan2@gmail.com','$token',NOW())";
 
 $result = $conn->query($sql);
-
-$user_id = mysqli_insert_id($conn);
 
 
 if ($result->num_rows > 0) {
 
 $location_id = 
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
-$sql = "INSERT INTO locations (user_id,district,state,country)
-VALUES('$user_id','pekan','pahang','malaysia')";
+$sql = "INSERT INTO locations (id,user_id,district,state,country)
+VALUES(22,21,'pekan','pahang','malaysia')";
 
 $data = $conn->query($sql);
 
--- insertdata.php
+++ insertdata.php
@@ -18,12 +18,12 @@ if ($conn->connect_error) {
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
 
-$user = "INSERT INTO user (name,email,token,created)
-VALUES('nadia','nadiashokkan2@gmail.com','$token',NOW())";
+$user = "INSERT INTO user (id,name,email,token,created)
+VALUES(21,'nadia','nadiashokkan2@gmail.com','$token',NOW())";
 $user_id = mysqli_insert_id($conn);
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
-$location = "INSERT INTO locations (user_id,district,state,country)
-VALUES('$user_id','pekan','pahang','malaysia')";
+$location = "INSERT INTO locations (id,user_id,district,state,country)
+VALUES(22,21,'pekan','pahang','malaysia')";
 
 
 
