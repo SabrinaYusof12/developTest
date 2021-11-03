ee0e36179d4a4c60dd5f596e024bb9dbacc7c61c
nadiashokkan <nadiashokkan1@gmail.com>
2021-11-03 17:31:01 +0800

    code

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ if($data->num_rows > 0){
   
 else {
     // set response code - 404 Not found
-    http_response_code(404);
+   // http_response_code(404);
     // tell the user no location found
  
         array("message" => "No location found.");
