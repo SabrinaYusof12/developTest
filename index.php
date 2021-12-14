c0b928263db5968df31eff313304cd592b8d194b
fyza <hafizah@mutiara-teknologi.com.my>
2021-12-14 15:20:41 +0800

    developer test

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$headers = 
+$headers = apache_request_headers();
 $token = NULL;
 foreach ($headers as $header => $value) {
 
@@ -27,49 +27,46 @@ foreach ($headers as $header => $value) {
 }
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
-$sql = 
-
+$sql = "SELECT * FROM users WHERE token ='$token'";
 $result = $conn->query($sql);
-
 
 if ($result->num_rows > 0) {
 
-$location_id = 
-//table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
-$sql = 
+    $location_id = ($_GET['location_id']);
+    //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
+    $sql = "SELECT district, state, country FROM locations WHERE id ='$location_id'";
 
-$data = $conn->query($sql);
+    $data = $conn->query($sql);
 
-  while($row = $data->fetch_assoc()) {
-     = $row["district"];
-     = $row["state"];
-     = $row["country"];
-  }
+    while($row = mysqli_fetch_assoc($data)) {
+        $array[] = $row;
+    }
 
-
-if($data->num_rows > 0){ 
-    // set response code - 200 OK
-  
-    // show products data
-         ($location);
-      }
-  
-else {
-    // set response code - 404 Not found
-  
-    // tell the user no location found
- 
-        array("message" => "No location found.")
-  
-}
+    if($data->num_rows > 0){ 
+        // set response code - 200 OK
+      
+        // show products data
+        echo json_encode($array);
+    }
+      
+    else {
+        // set response code - 404 Not found
+      
+        // tell the user no location found
+        $msg = "No location found.";
+        echo $msg;
+            // array("message" => "No location found.");
+      
+    }
 
 } else {
     // set response code - 401 401 Unauthorized
 
   
     // no user found
- 
-        array("message" => "401 Unauthorized.")
+        $msg = "401 Unauthorized.";
+        echo $msg;
+        // array("message" => "401 Unauthorized.");
    
 
 }
