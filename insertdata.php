2e75eacfd87f38998a6aab54439630d272ae46fc
developer-test <developer.test@hla-integrated.com>
2022-01-07 07:39:18 +0000

    Update insertdata.php

--- insertdata.php
+++ insertdata.php
@@ -7,7 +7,7 @@ $username = "developertes";
 $password = "HL@2021test";
 $dbname = "hla";
 
-//
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -17,9 +17,20 @@ if ($conn->connect_error) {
 }
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
-$user = 
+$user = "CREATE TABLE Users (
+  id INT(6) UNSIGNED AUTO-INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  token VARCHAR (255),
+  created DATETIME
+  )";
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
-$location = 
+$location = "CREATE TABLE Locations (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  district VARCHAR(255),
+  state VARCHAR(255),
+  country VARCHAR(255)
+  )";
 
 
 
