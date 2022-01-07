ba0dd293e1fbae06819bf3439e7f3aaa5ba2d09e
developer-test <developer.test@hla-integrated.com>
2022-01-07 09:41:32 +0000

    Update insertdata.php

--- insertdata.php
+++ insertdata.php
@@ -17,9 +17,9 @@ if ($conn->connect_error) {
 }
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
-$user = 
+$user = "INSERT INTO Users (userID, Name, Email, Token, Created) VALUES ('01', 'Nurul Syaheeda', 'syeed98@gmail.com', 'MD5', 7-1-2022 16:00'"
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
-$location = 
+$location = "INSERT INTO Locations (locationID, district, state, country) VALUES ('02', 'BBST', 'Sepang', 'Selangor'"
 
 
 
