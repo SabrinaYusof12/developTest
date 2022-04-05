88a6b3983617f9ba98ed4f96f44ba5433a05ce58
anzari <anzari@itools.com.my>
2022-04-05 15:44:26 +0800

    test

--- index.php
+++ index.php
@@ -1,80 +1,83 @@
 <?php
 
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
-// required headers
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+echo "string";
+exit();
+
+// ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+// // required headers
+// header("Access-Control-Allow-Origin: *");
+// header("Content-Type: application/json; charset=UTF-8");
   
-// include database and object files
+// // include database and object files
 
-$servername = "localhost";
-$username = "developertest";
-$password = "HL@2021test";
-$dbname = "hla";
+// $servername = "localhost";
+// $username = "developertest";
+// $password = "HL@2021test";
+// $dbname = "hla";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
 
-$headers = 
-$token = NULL;
-foreach ($headers as $header => $value) {
+// $headers = 
+// $token = NULL;
+// foreach ($headers as $header => $value) {
 
-    if($header == 'Authorization'){
-        $token = $value;
-    }
-}
+//     if($header == 'Authorization'){
+//         $token = $value;
+//     }
+// }
 
-//table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
-$sql = 
+// //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
+// $sql = 
 
-$result = $conn->query($sql);
-
-
-if ($result->num_rows > 0) {
-
-$location_id = 
-//table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
-$sql = 
-
-$data = $conn->query($sql);
-
-  while($row = $data->fetch_assoc()) {
-     = $row["district"];
-     = $row["state"];
-     = $row["country"];
-  }
+// $result = $conn->query($sql);
 
 
-if($data->num_rows > 0){ 
-    // set response code - 200 OK
+// if ($result->num_rows > 0) {
+
+// $location_id = 
+// //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
+// $sql = 
+
+// $data = $conn->query($sql);
+
+//   while($row = $data->fetch_assoc()) {
+//      = $row["district"];
+//      = $row["state"];
+//      = $row["country"];
+//   }
+
+
+// if($data->num_rows > 0){ 
+//     // set response code - 200 OK
   
-    // show products data
-         ($location);
-      }
+//     // show products data
+//          ($location);
+//       }
   
-else {
-    // set response code - 404 Not found
+// else {
+//     // set response code - 404 Not found
   
-    // tell the user no location found
+//     // tell the user no location found
  
-        array("message" => "No location found.")
+//         array("message" => "No location found.")
   
-}
+// }
 
-} else {
-    // set response code - 401 401 Unauthorized
+// } else {
+//     // set response code - 401 401 Unauthorized
 
   
-    // no user found
+//     // no user found
  
-        array("message" => "401 Unauthorized.")
+//         array("message" => "401 Unauthorized.")
    
 
-}
+// }
 
-$conn->close();
+// $conn->close();
 ?>
