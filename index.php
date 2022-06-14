1a827d4dc4e53a2b88f865db992037f145bccc77
developer-test <developer.test@hla-integrated.com>
2022-06-14 16:45:09 +0000

    Update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+//ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
