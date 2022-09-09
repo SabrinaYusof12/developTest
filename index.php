55fbb659346adb6d6a79144f16a6c40e434adc00
Edham Rabuan <edham@vimigoapp.com>
2022-09-09 18:14:13 +0800

    complete all missing code

--- index.php
+++ index.php
@@ -16,10 +16,10 @@ $dbname = "hla";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-$headers = 
+$headers = apache_request_headers();
 $token = NULL;
 foreach ($headers as $header => $value) {
 
@@ -29,51 +29,44 @@ foreach ($headers as $header => $value) {
 }
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
-$sql = 
+$sql = "SELECT * FROM users WHERE token='".$token."'";
 
 $result = $conn->query($sql);
 
 
 if ($result->num_rows > 0) {
 
-$location_id = 
-//table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
-$sql = 
+    $location_id = $_GET['location_id'];
+    //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
+    $sql = "SELECT * FROM locations WHERE id='".$location_id."'";
 
-$data = $conn->query($sql);
+    $data = $conn->query($sql);
 
-  while($row = $data->fetch_assoc()) {
-     = $row["district"];
-     = $row["state"];
-     = $row["country"];
-  }
+    $location = array();
+    while($row = $data->fetch_assoc()) {
+        $location['district'] = $row["district"];
+        $location['state'] = $row["state"];
+        $location['country'] = $row["country"];
+    }
 
-
-if($data->num_rows > 0){ 
-    // set response code - 200 OK
-  
-    // show products data
-         ($location);
-      }
-  
-else {
-    // set response code - 404 Not found
-  
-    // tell the user no location found
- 
-        array("message" => "No location found.")
-  
-}
-
+    if($data->num_rows > 0){ 
+        // set response code - 200 OK
+        http_response_code(200);
+        // show products data
+        print json_encode($location);
+    } else {
+        // set response code - 404 Not found
+        http_response_code(404);
+    
+        // tell the user no location found
+        print json_encode(array("message" => "No location found."));
+    }
 } else {
     // set response code - 401 401 Unauthorized
+    http_response_code(401);
 
-  
     // no user found
- 
-        array("message" => "401 Unauthorized.")
-   
-
+    print json_encode(array("message" => "401 Unauthorized."));
 }
 
 $conn->close();
