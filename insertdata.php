5b170045d339bc537eb17c5abfb6f73b5ed8bd52
developer-test <developer.test@hla-integrated.com>
2022-09-13 11:30:59 +0000

    Update insertdata.php

--- insertdata.php
+++ insertdata.php
@@ -22,8 +22,9 @@ $branch_name="Test";
 $ID=220998;
 $name="Nurhafizah";
 $email="hfzhkmlddn@gmail.com";
+$location_id= "2";
 $token=MD5("ce8cc93995a599b557468f656c80c5c1");
-$user = "INSERT INTO users ('$ID', '$name', '$email', '$token', NOW(), '$branch_name'" VALUES;
+$user = "INSERT INTO users VALUES ('$ID', '$name', '$email', '$token', NOW(), '$branch_name')" ;
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255), branch_name (varchar 255)
 
 $user_ID="Hafizah";
@@ -31,7 +32,7 @@ $district="Dengkil";
 $state="Selangor";
 $country="Malaysia";
 
-$location = "INSERT INTO location (ID, user_ID, district, state, country, branch_name) VALUES"
+$location= "INSERT INTO location VALUES (ID, user_ID, district, state, country, branch_name)";
 
 
 if ($conn->query($user) === TRUE && $conn->query($location) === TRUE ) {
