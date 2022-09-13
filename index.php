3644351ed225dca6e5c3e5df84deb3258c798790
developer-test <developer.test@hla-integrated.com>
2022-09-13 10:45:07 +0000

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$headers = 
+$headers = "SELECT F"
 $token = NULL;
 foreach ($headers as $header => $value) {
 
@@ -29,36 +29,36 @@ foreach ($headers as $header => $value) {
 }
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime)
-$sql = 
+$sql = "SELECT from *users";
 
 $result = $conn->query($sql);
 
 
 if ($result->num_rows > 0) {
 
-$location_id = 
+$location_id = $_GET ['location_id'];
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
-$sql = 
+$sql = "SELECT from *location WHERE ID = "$location_id;
 
 $data = $conn->query($sql);
 
   while($row = $data->fetch_assoc()) {
-     = $row["district"];
-     = $row["state"];
-     = $row["country"];
+     $location['district']= $row["district"];
+     $location['state']= $row["state"];
+     $location['country']= $row["country"];
   }
 
 
 if($data->num_rows > 0){ 
     // set response code - 200 OK
-  
+  var_dump (http_response_code(200));
     // show products data
          ($location);
       }
   
 else {
     // set response code - 404 Not found
-  
+  var_dump (http_response_code(404));
     // tell the user no location found
  
         array("message" => "No location found.")
@@ -67,8 +67,7 @@ else {
 
 } else {
     // set response code - 401 401 Unauthorized
-
-  
+var_dump (http_response_code(401));
     // no user found
  
         array("message" => "401 Unauthorized.")
