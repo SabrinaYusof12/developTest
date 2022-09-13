50b5900368ff643f57e024ab3c2644481f4a8f18
Amirah Safina <amirahsafina@gmail.com>
2022-09-14 00:30:48 +0800

    commit

--- insertdata.php
+++ insertdata.php
@@ -1,6 +1,8 @@
 <?php
 
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // include database 
 
@@ -20,14 +22,14 @@ if ($conn->connect_error) {
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime), branch_name (varchar 255)
 $user = "INSERT INTO users (id, name, email, token, created, branch_name)
-VALUES ('1221', 'safina', 'safina@example.com', ".md5("safina").", ". date("Y.m.d").", 'safina')";
+VALUES ('1221', 'safina', 'safina@example.com', '" . md5("safina") . "', '" . date("Y.m.d") . "', 'safina')";
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255), branch_name (varchar 255)
 $location = "INSERT INTO locations (id, user_id, district, state, country, branch_name)
 VALUES ('01', '1221', 'shah alam', 'selangor', 'malaysia', 'safina')";
 
 
 
-if ($conn->query($user) === TRUE && $conn->query($location) === TRUE ) {
+if ($conn->query($user) === TRUE && $conn->query($location) === TRUE) {
   $last_id = $conn->insert_id;
   //get location_id and token value
   echo $location_id;
@@ -38,5 +40,3 @@ if ($conn->query($user) === TRUE && $conn->query($location) === TRUE ) {
 }
 
 $conn->close();
-
-?>
