bf1a0864a6514d852e5317b79d3b24bbd8831487
Amirah Safina <amirahsafina@gmail.com>
2022-09-14 00:51:11 +0800

    commit

--- insertdata.php
+++ insertdata.php
@@ -22,10 +22,10 @@ if ($conn->connect_error) {
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime), branch_name (varchar 255)
 $user = "INSERT INTO users (id, name, email, token, created, branch_name)
-VALUES ('1221', 'safina', 'safina@example.com', '" . md5("safina") . "', '" . date('Y-m-d H:i:s') . "', 'safina')";
+VALUES (1221, 'safina', 'safina@example.com', '" . md5("safina") . "', '" . date('Y-m-d H:i:s') . "', 'safina')";
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255), branch_name (varchar 255)
 $location = "INSERT INTO locations (id, user_id, district, state, country, branch_name)
-VALUES ('01', '1221', 'shah alam', 'selangor', 'malaysia', 'safina')";
+VALUES (1, 1221, 'shah alam', 'selangor', 'malaysia', 'safina')";
 
 
 
@@ -36,7 +36,7 @@ if ($conn->query($user) === TRUE && $conn->query($location) === TRUE) {
   echo $token;
   echo "New record created successfully";
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "Error: ";
 }
 
 $conn->close();
