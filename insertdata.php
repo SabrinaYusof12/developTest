040903a80a64b477087ef76457dab55e6c95dca0
developer-test <developer.test@hla-integrated.com>
2022-11-08 12:48:22 +0000

    Replace insertdata.php

--- insertdata.php
+++ insertdata.php
@@ -19,9 +19,9 @@ if ($conn->connect_error) {
 }
 
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime), branch_name (varchar 255)
-$user = 
+$user = "INSERT INTO users (id, name, email, token, created, branch_name) VALUES (1, 'ainmokhtar' 'norainmokhtar88@gmail.com', CONVERT(VARCHAR(255), HashBytes('MD5', 'test'),2), Getdate(), 'ainmokhtar')"
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255), branch_name (varchar 255)
-$location = 
+$location = "INSERT INTO locations (id, user_id, district, state, country, branch_name) VALUES (1, 1, 'Jeli', 'Kelantan', 'Malaysia', 'ainmokhtar')"
 
 
 
