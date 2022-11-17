299cffd1d401db06a143810d1894c4398530a156
developer-test <developer.test@hla-integrated.com>
2022-11-17 13:30:28 +0000

    Update insertdata.php

--- insertdata.php
+++ insertdata.php
@@ -21,7 +21,7 @@ if ($conn->connect_error) {
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime), branch_name (varchar 255)
 
 $user = "INSERT INTO Users (name, email, token, created DATETIME(),branch_name) 
-VALUES ('1', 'Syukri', 'Syukri@mail.com','MD598syukribolang123456789' ,'DATETIME()','SIP')";
+VALUES ('1', 'Syukri', 'Syukri@mail.com','testsyukri' ,'DATETIME()','SIP')";
 
 
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255), branch_name (varchar 255)
