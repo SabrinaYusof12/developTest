041231b476020157ecdebd9f526b205c58e069b8
sabrinayusof <you@example.com>
2023-04-04 10:44:42 +0800

    Update Index & InsertData

--- insertdata.php
+++ insertdata.php
@@ -18,10 +18,20 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+$name =$_POST['name']; 
+$email =$_POST['email']; 
+$token =md5($_POST['token']); 
+$created =$_POST['created'];
+$user_id =$_POST['user_id'];
+$district =$_POST['district'];
+$state =$_POST['state'];
+$country =$_POST['country'];
+$branch_name =$_POST['branch_name'];
+
 //table users - id (int), name (varchar 255), email (varchar 255), token (MD5 varchar 255), created (datetime), branch_name (varchar 255)
-$user = 
+$user = ("INSERT INTO users (id, name, email, token, created, branch_name) VALUES ('NULL', '$name', '$email', MD5('$token'), '$created', '$branch_name')");
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255), branch_name (varchar 255)
-$location = 
+$location = ("INSERT INTO locations (id, user_id, district, state, country, branch_name ('NULL','$user_id', '$district', '$state', '$country', '$branch_name'");
 
 
 
